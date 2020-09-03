16522c73482f0c253f31963037955146d1d2792c
Vladimír Zdražil <mail@vzdrazil.com>
2020-09-03 07:29:04 +0200

    add solution

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -1,4 +1,20 @@
-module Pangram (isPangram) where
+module Pangram
+  ( isPangram
+  )
+where
+
+import           Data.List                      ( sort
+                                                , nub
+                                                )
+import           Data.Char                      ( isAscii
+                                                , toLower
+                                                , isLetter
+                                                )
 
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram text =
+  let alpha = ['a' .. 'z']
+      newText =
+          nub . sort . map toLower . filter isLetter . filter isAscii $ text
+  in  alpha == newText
+
