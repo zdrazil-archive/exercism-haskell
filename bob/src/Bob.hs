a87d2483bedec1de7b62ffa0edf928b844b02485
Vladimír Zdražil <mail@vzdrazil.com>
2020-09-05 16:43:22 +0200

    improve based on community

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -12,17 +12,27 @@ isUpper :: Text -> Bool
 isUpper a | alphaChars == "" = False
           | otherwise        = T.all Char.isUpper alphaChars
   where alphaChars = T.filter Char.isAlpha a
--- responseFor a = T.init a
 
-responseFor :: Text -> Text
-responseFor a | T.null withoutSpaces = "Fine. Be that way!"
-              | isU && isQuestion    = "Calm down, I know what I'm doing!"
-              | isQuestion           = "Sure."
-              | isU                  = "Whoa, chill out!"
-              | otherwise            = "Whatever."
+data Category = ShoutQuestion | Silence | Question | Shout | Other
+
+getResponse :: Category -> Text
+getResponse Silence       = "Fine. Be that way!"
+getResponse ShoutQuestion = "Calm down, I know what I'm doing!"
+getResponse Question      = "Sure."
+getResponse Shout         = "Whoa, chill out!"
+getResponse Other         = "Whatever."
+
+categorizeStatement :: Text -> Category
+categorizeStatement a | isSilence             = Silence
+                      | isShout && isQuestion = ShoutQuestion
+                      | isQuestion            = Question
+                      | isShout               = Shout
+                      | otherwise             = Other
  where
   withoutSpaces = T.filter (not . Char.isSpace) a
   isQuestion    = T.isSuffixOf "?" withoutSpaces
-  isU           = isUpper withoutSpaces
+  isShout       = isUpper withoutSpaces
+  isSilence     = T.null withoutSpaces
 
-
+responseFor :: Text -> Text
+responseFor = getResponse . categorizeStatement
