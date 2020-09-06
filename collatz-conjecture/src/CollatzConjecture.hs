0dd6cd3e0f8bbcb9409dfcd66c9e5d4330bf8f94
Vladimír Zdražil <mail@vzdrazil.com>
2020-09-06 17:37:35 +0200

    add solution

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -3,5 +3,12 @@ module CollatzConjecture
   )
 where
 
+method :: Integer -> Integer -> (Integer, Integer)
+method n count | n == 1    = (n, count)
+               | even n    = method (n `div` 2) (count + 1)
+               | odd n     = method ((3 * n) + 1) (count + 1)
+               | otherwise = (n, count)
+
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n | n >= 1    = Just (snd $ method n 0)
+          | otherwise = Nothing
