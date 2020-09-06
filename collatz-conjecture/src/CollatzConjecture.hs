27656532d108c9bb67f8441f327539ee69f8dcd9
Vladimír Zdražil <mail@vzdrazil.com>
2020-09-06 18:16:39 +0200

    add solution

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -3,12 +3,15 @@ module CollatzConjecture
   )
 where
 
-method :: Integer -> Integer -> (Integer, Integer)
-method n count | n == 1    = (n, count)
-               | even n    = method (n `div` 2) (count + 1)
-               | odd n     = method ((3 * n) + 1) (count + 1)
-               | otherwise = (n, count)
+next :: Integer -> Integer
+next n | n == 1    = n
+       | even n    = n `div` 2
+       | odd n     = (3 * n) + 1
+       | otherwise = n
+
+calc :: Integer -> [Integer]
+calc n = takeWhile (/= 1) . iterate next $ n
 
 collatz :: Integer -> Maybe Integer
-collatz n | n >= 1    = Just (snd $ method n 0)
+collatz n | n >= 1    = Just . fromIntegral . length . calc $ n
           | otherwise = Nothing
