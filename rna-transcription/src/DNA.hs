197787e19f3e83404adf1654f2f1d72a79b13ede
Vladimír Zdražil <mail@vzdrazil.com>
2020-09-07 08:07:21 +0200

    wip rna

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,4 +1,26 @@
-module DNA (toRNA) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module DNA
+  ( toRNA
+  )
+where
+
+dna = "acgt"
+rna = "acgu"
+
+flippedElem :: String -> Char -> Bool
+flippedElem = flip elem
+
+isValid :: String -> String -> Bool
+isValid strand a | null a    = False
+                 | otherwise = all (flippedElem strand) a
+
+isValidDna :: String -> Bool
+isValidDna = isValid dna
+
+isValidRna :: String -> Bool
+isValidRna = isValid rna
+
 
 toRNA :: String -> Either Char String
 toRNA xs = error "You need to implement this function."
