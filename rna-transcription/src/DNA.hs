5e4b5f106e8506002cab2ff550d87eebcb0f527a
Vladimír Zdražil <mail@vzdrazil.com>
2020-09-09 08:52:46 +0200

    pass tests

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -5,29 +5,26 @@ module DNA
   )
 where
 
-dna = "acgt"
-rna = "acgu"
+isDnaNucleotide :: Char -> Bool
+isDnaNucleotide a = elem a ("GCTA" :: String)
 
-flippedElem :: String -> Char -> Bool
-flippedElem = flip elem
-
-isValid :: String -> String -> Bool
-isValid strand a | null a    = False
-                 | otherwise = all (flippedElem strand) a
-
-isValidDna :: String -> Bool
-isValidDna = isValid dna
+getInvalidNucleotides :: String -> String
+getInvalidNucleotides a = filter (not . isDnaNucleotide) a
 
 dnaToRnaNucleotide :: Char -> Char
 dnaToRnaNucleotide a = case a of
-  'g' -> 'c'
-  'c' -> 'g'
-  't' -> 'a'
-  'a' -> 'u'
+  'G' -> 'C'
+  'C' -> 'G'
+  'T' -> 'A'
+  'A' -> 'U'
+  _   -> a
 
 dnaToRna :: String -> String
 dnaToRna = map dnaToRnaNucleotide
 
-
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA xs =
+  let invalids = getInvalidNucleotides $ xs
+  in  case invalids of
+        [] -> Right $ dnaToRna xs
+        _  -> Left . head $ invalids
