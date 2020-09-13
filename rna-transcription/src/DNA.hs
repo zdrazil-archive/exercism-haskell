26e6162b7628c33b30f8ca28656bb05360e2fc1d
Vladimír Zdražil <mail@vzdrazil.com>
2020-09-13 15:27:38 +0200

    refactor

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -3,18 +3,13 @@ module DNA
   )
 where
 
-dnaToRnaChar :: Char -> Either Char Char
-dnaToRnaChar a = case a of
+toRNA' :: Char -> Either Char Char
+toRNA' a = case a of
   'G' -> Right 'C'
   'C' -> Right 'G'
   'T' -> Right 'A'
   'A' -> Right 'U'
   _   -> Left a
 
-dnaToRnaString :: String -> Either Char String
-dnaToRnaString = mapM dnaToRnaChar
-
 toRNA :: String -> Either Char String
-toRNA xs = case dnaToRnaString xs of
-  Left  symbol -> Left symbol
-  Right dna    -> Right dna
+toRNA = mapM toRNA'
