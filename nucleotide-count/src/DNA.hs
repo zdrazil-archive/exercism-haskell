25ed9c94b4620174edad02aea6b888c46f0cd061
Vladimír Zdražil <mail@vzdrazil.com>
2020-09-20 11:12:18 +0200

    add solution

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -4,9 +4,28 @@ module DNA
   )
 where
 
-import           Data.Map                       ( Map )
+import           Data.Map                       ( Map
+                                                , fromList
+                                                )
+import           Data.List
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
+charToNucleotide :: Char -> Either Char Nucleotide
+charToNucleotide a = case a of
+  'A' -> Right A
+  'C' -> Right C
+  'G' -> Right G
+  'T' -> Right T
+  _   -> Left a
+
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts xs = case mapM charToNucleotide xs of
+  Left invalidNucleotide -> Left [invalidNucleotide]
+  Right nucleotides ->
+    Right
+      . fromList
+      . map (\a -> (head a, length a))
+      . group
+      . sort
+      $ nucleotides
